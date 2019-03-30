1e91e16971e52b3209e4d80d2317156ef515a05c
errorreplicator <error.replicator@gmail.com>
2019-03-30 20:02:47 +0100

    save and load weights

--- data/dogcat.py
+++ data/dogcat.py
@@ -59,3 +59,36 @@ def load_data (input_size=3000,resolution=50,test_data=True):
         print(f'size of {catalogs[1]} table:',len(y_train))
 
         return (X_train,y_train)
+
+def load_TestData (resolution):
+    path = 'C:\Dataset\img\Test'
+    catalogs = ['Dog', 'Cat']
+    resolution = resolution
+    catDog_list = []
+
+    for folder in catalogs:
+        index = 0
+        fol_path = os.path.join(path, folder)
+        for file in os.listdir(fol_path):
+            try:
+                image = cv2.imread(os.path.join(fol_path, file))  # ,cv2.IMREAD_GRAYSCALE)
+                grey = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            except Exception as e:
+                print(f'Error at file index {index} with path:', fol_path, '\\', file, sep='')
+                pass
+
+            image_resize = cv2.resize(grey, (resolution, resolution))
+            catDog_list.append([image_resize, catalogs.index(folder)])
+            index += 1
+            # if index > input_size - 1: break
+    X_test= []
+    y_test = []
+
+    random.shuffle(catDog_list)
+    for x, y in catDog_list:
+        X_test.append(x)
+        y_test.append(y)
+    print(f'size of {catalogs[0]} table:', len(X_test))
+    print(f'size of {catalogs[1]} table:', len(y_test))
+
+    return (X_test, y_test)
--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-from models import manipulation
+from models import manipulation, kerasmodels
+from data import dogcat
+from tasking import general
 
-model = manipulation.loadModel('repo/testowy1.20190325205434.h5')
+resolution = 50
+epoch = 10
+input_size = 3000
+shape = (resolution,resolution,1)
+X_train,y_train,X_test,y_test = dogcat.load_data(input_size=input_size,resolution=resolution,test_data=True)
+X_train = general.simple_reshape(X_train)
+X_train = general.simple_norm(X_train)
 
-manipulation.modelSummary(model)
+model_trained = kerasmodels.modelSeq1(shape)
+model_trained.fit(X_train,y_train,batch_size=50,epochs=epoch,validation_split=0.2 )
+
+model_raw = kerasmodels.modelSeq1(shape)
+model_raw.load_weights('repo/CatDog1_w.h5')
+
+model_saved = manipulation.loadFileModel('repo/CatDog1.20190330194006.h5')
+
+
+
+
