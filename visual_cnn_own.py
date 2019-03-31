a22df3ceb02bac503976d85762c1bb352427f953
errorreplicator <error.replicator@gmail.com>
2019-03-31 19:06:01 +0200

    save and load weights

--- visual_cnn_own.py
+++ visual_cnn_own.py
@@ -1,30 +1,48 @@
-from models import kerasmodels, manipulation
+from models import manipulation, kerasmodels
 from data import dogcat
 from tasking import general
 
 resolution = 50
 epoch = 10
 input_size = 3000
-
-X_train,y_train,x_test,y_test = dogcat.load_data(input_size,resolution)
+shape = (resolution,resolution,1)
+X_train,y_train,X_test,y_test = dogcat.load_data(input_size=input_size,resolution=resolution,test_data=True)
 X_train = general.simple_reshape(X_train)
-x_test = general.simple_reshape(X_train)
 X_train = general.simple_norm(X_train)
-x_test = general.simple_norm(x_test)
+
+model_trained = kerasmodels.modelSeq1(shape)
+model_trained.fit(X_train,y_train,batch_size=50,epochs=epoch,validation_split=0.2 )
+
+model_raw = kerasmodels.modelSeq1(shape)
+model_raw.load_weights('repo/CatDog1_w.h5')
+
+model_saved = manipulation.loadFileModel('repo/CatDog1.20190330194006.h5')
+X_test, y_test = dogcat.load_TestData(50)
+X_test = general.simple_reshape(X_test)
+X_test = general.simple_norm(X_test)
+
+predictions_m_trained = model_trained.predict(X_test)
+prediction_m_raw = model_raw.predict(X_test)
+predictions_m_saved = model_saved.predict(X_test)
+
+print(f'Number of test samples:{len(y_test)}',sep='\n')
+
+print('predictions_m_trained below', sep='\n')
+for x in predictions_m_trained:
+    print(x)
+print('')
+
+print('predictions_m_raw below',sep='\n')
+for y in prediction_m_raw:
+    print(y)
+print('')
+
+print('predictions_m_saved below',sep='\n')
+for z in predictions_m_saved:
+    print(z)
+print('')
+
+print(y_test)
 
 
-model = kerasmodels.modelSeq1(X_train.shape[1:])
-# model = kerasmodels.modelSeq1((50,50,1))
 
-# model.load_weights('repo/weights.h5')
-model.fit(X_train,y_train,validation_split=0.2 ,epochs=epoch,batch_size=50)
-# model = manipulation.loadWeights(model,'repo/weights.h5')
-manipulation.saveModelAll(model,'CatDog1')
-manipulation.saveWeights(model,'CatDog1_w')
-#1 /255 loss: 0.2324 - acc: 0.9078
-#2 /255 loss: 0.3290 - acc: 0.8525
-#3 /255 loss: 0.3129 - acc: 0.8640
-
-#1 keras.utils.normalize loss: 0.4403 - acc: 0.7928
-#2 loss: 0.3445 - acc: 0.8468
-#3 loss: 0.4183 - acc: 0.8058
