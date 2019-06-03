9b14c86ba77b206baad64f836844b0ba26be238d
errorreplicator <error.replicator@gmail.com>
2019-06-03 15:32:15 +0200

    vgg16 dogbreeds -- results

--- test4.py
+++ test4.py
@@ -1,40 +1,80 @@
-from data import dataload
-from tasking import general
+
+import pandas as pd
 import numpy as np
 
-
-X_train, y_train = dataload.dogbreedsD(5,resolution=224,test_data=False)
-
-print(type(X_train))
-X_train = np.array(X_train)
-print(X_train.shape)
-print(X_train[0].shape)
-X_train = general.simple_reshape_color(X_train,224)
-print()
-print(X_train.shape)
-print(X_train[0].shape)
-print()
-X_2nd, y_2nd = dataload.load_data(input_size=5,test_data=False)
-
-X_2nd = np.array(X_2nd)
-print(X_2nd.shape)
-print(X_2nd[0].shape)
-X_2nd = general.simple_reshape(X_2nd,50)
-print()
-print(X_2nd.shape)
-print(X_2nd[0].shape)
-
-# X_train = general.simple_reshape_color(X_train,224)
-# X_train = general.simple_norm(X_train)
-
-# print(X_train[0])
-# print(20*'*')
-print(X_2nd[0])
-
-X_train = general.simple_norm(X_train)
-X_2nd = general.simple_norm(X_2nd)
+input_size = 3000
+resolution = 224
+test_data = True
 
 
-print(X_train[0])
-print(200*'*')
-print(X_2nd[0])
+PATH = 'c:/Dataset/dogbreeds/'
+labels_file = pd.read_csv(f'{PATH}labels.csv')
+dataset = labels_file
+print(dataset.head())
+dataset['path'] = [f'{PATH}train/{x}.jpg' for x in dataset['id']]
+print(dataset.shape)
+print(dataset.head())
+
+# dataset3 = dataset.loc[]
+
+# breed_names = dataset['breed'].unique()
+#
+#
+# # dict1 = {y:x for y,x in enumerate(tmp)}
+# dict2 = {x: y for y, x in enumerate(breed_names)}
+#
+# dataset['bin_breeds'] = [dict2[x] for x in dataset['breed']]
+#
+# def get_vector(idx):
+#     zeroV = np.zeros((120,), dtype=int)
+#     zeroV[idx['bin_breeds']] = 1
+#     return zeroV
+#
+# dataset['vector'] = dataset.apply(get_vector, axis=1)
+# index = 0
+# full_list = []
+# X_train = []
+# y_train = []
+# X_test = []
+# y_test = []
+#
+# for idx, row in dataset.iterrows():
+#     try:
+#         image = cv2.imread(row['path'])#,cv2.IMREAD_GRAYSCALE)
+#         # grey = cv2.cvtColor(image)#, cv2.COLOR_RGB2GRAY)
+#     except Exception as e:
+#         print(f'Error at file index {index} with path:', row['path'], sep='')
+#         pass
+#
+#     image_resize = cv2.resize(image, (resolution, resolution))
+#     full_list.append([image_resize, row['vector']])
+#     index += 1
+#     if index > input_size: break
+#     if index%500==0:print(index)
+#
+# random.shuffle(full_list)
+# index = 0
+# if test_data == True:
+#     for x, y in full_list:
+#         index += 1
+#         if index % 10 != 0:
+#             X_train.append(x)
+#             y_train.append(y)
+#         else:  # every 10th sample is a test sample
+#             X_test.append(x)
+#             y_test.append(y)
+#
+#     # print(f'Overall table size:', len(y_test))
+#     print('Please remember to normalize and reshape || simple_norm and simple_reshape')
+#
+#     return (X_train, np.array(y_train), X_test, np.array(y_test))
+#
+# else:
+#
+#     for x, y in full_list:
+#         X_train.append(x)
+#         y_train.append(y)
+#     # print(f'Overall table size:', len(y_test))
+#     print('Please remember to normalize and reshape || simple_norm and simple_reshape')
+#
+#     return (X_train, np.array(y_train))
